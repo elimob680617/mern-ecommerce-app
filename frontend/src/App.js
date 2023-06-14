58b9d736030b273d7dc53cfbf063f5283143812c
Elham Mobini <elhamobini@gmail.com>
2023-06-14 14:38:46 +0330

    set custom theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome to proshop</h1>
+          <Button variant="warning"> test</Button>
         </Container>
       </main>
       <Footer />
