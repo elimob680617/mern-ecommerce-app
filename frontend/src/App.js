2bb42ed0deb17eccd75a4c27b5655a56cd4aa1e5
Elham Mobini <elhamobini@gmail.com>
2023-06-15 18:48:35 +0330

    add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
